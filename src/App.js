fdb9c64ef9e6d996d4b30a72976941bb12a05e19
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-29 16:21:41 -0500

    built standar header. Pending modal navigation and nav menu

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NewUser from './components/NewUser/NewUser';
 import DoPage from './pages/DoPage/DoPage'
 import Header from './components/Header/Header';
 import { useEffect, useState } from 'react';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
       <BrowserRouter>
         <Header isAuth={isAuth} />
         <Switch>
+          <Route exact path='/' component={HomePage} />
           <Route path='/contactus' render={(props) => <ContactPage isAuth={isAuth} {...props} />}/>
           <Route path='/register' component={NewUser} />
           <Route path='/do' component={DoPage} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,22 +2,26 @@ import './Header.scss';
 import { Cross as Hamburger } from 'hamburger-react'
 import { useState } from 'react';
 
-function Header({isAuth}) {
-    
-    const [showNav, setShowNav] = useState(false)
-    
-    return ( 
-        <header className='header'>
-            <div className='header__top'>
-                <Hamburger toggled={showNav} toggle={setShowNav} />
-                <span className='header__logo'>My Kushki</span>
-                <span className='header__auth'>{isAuth ? 'Sign Out' : 'Sign In'}</span>
-            </div>
-            <nav className='header__nav'>
+function Header({ isAuth }) {
 
-            </nav>
-        </header>
-     );
+    const [showNav, setShowNav] = useState(false)
+
+    return (
+        <div className='container'>
+            <header className='header'>
+                <div className='header__top'>
+                    <span className='header__logo'>My Kushki</span>
+                    <div className='header__top-right'>
+                        <span className='header__auth'>{isAuth ? 'Sign Out' : 'Sign In'}</span>
+                        <Hamburger toggled={showNav} toggle={setShowNav} color='#fff' rounded label='Show navigation menu' />
+                    </div>
+                </div>
+                <nav className='header__nav'>
+
+                </nav>
+            </header>
+        </div>
+    );
 }
 
 export default Header;
