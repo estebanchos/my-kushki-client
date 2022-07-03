d05c998e441d203d25f225c013b573aaef1e547f
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-03 14:35:28 -0500

    added static resources for the learning articles

---
+++ src/assets/staticdata/articlesBasics.js
@@ -0,0 +1,23 @@
+import wsLogo from '../logos/ws.svg'
+import investopediaLogo from '../logos/investopedia.svg'
+
+export const budgetBasics = [
+    {
+        source: 'Wealthsimple',
+        title: 'How to Make a Budget',
+        url: 'https://www.wealthsimple.com/en-ca/learn/how-to-make-a-budget',
+        logo: { wsLogo }
+    },
+    {
+        source: 'Wealthsimple',
+        title: 'Budgeting: What It Is and How to Do It',
+        url: 'https://www.wealthsimple.com/en-ca/learn/what-is-budgeting',
+        logo: { wsLogo }
+    },
+    {
+        source: 'Investopedia',
+        title: 'How To Invest on a Shoestring Budget',
+        url: 'https://www.investopedia.com/articles/younginvestors/07/shoestring_budget.asp',
+        logo: { investopediaLogo }
+    },
+]
