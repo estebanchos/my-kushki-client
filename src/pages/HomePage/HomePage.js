b0054340d7b2b9a5e79f53038eed4f3f807ef1be
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-04 15:14:44 -0500

    fixed hover styles and other globals

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,6 +7,8 @@ import house from '../../assets/images/house.png';
 import invest from '../../assets/images/invest.png';
 import retirement from '../../assets/images/retirement.png';
 import taxes from '../../assets/images/taxes.png';
+import 'antd/dist/antd.css';
+import { Carousel } from 'antd';
 
 function HomePage() {
     return (
@@ -22,22 +24,24 @@ function HomePage() {
                 <div className='questions'>
                     <h3 className='questions__title'>Be ready to make your financial decisions</h3>
                     <div className='questions__gallery'>
-                        <div className='questions__container'>
-                            <img className='questions__image' src={house} alt='person working with a small model of a house' />
-                            <p className='questions__question'>Buying or renting a place?</p>
-                        </div>
-                        <div className='questions__container'>
-                            <img className='questions__image' src={invest} alt='person working with a small model of a house' />
-                            <p className='questions__question'>Investing by myself or hiring someone?</p>
-                        </div>
-                        <div className='questions__container'>
-                            <img className='questions__image' src={retirement} alt='person working with a small model of a house' />
-                            <p className='questions__question'>Saving for retirement or education?</p>
-                        </div>
-                        <div className='questions__container'>
-                            <img className='questions__image' src={taxes} alt='person working with a small model of a house' />
-                            <p className='questions__question'>Using accounts that have tax advantages?</p>
-                        </div>
+                        <Carousel autoplay autoplaySpeed={5000} dots={false}>
+                            <div className='questions__container'>
+                                <img className='questions__image' src={house} alt='person working with a small model of a house' />
+                                <p className='questions__question'>Buying or renting a place?</p>
+                            </div>
+                            <div className='questions__container'>
+                                <img className='questions__image' src={invest} alt='person working with a small model of a house' />
+                                <p className='questions__question'>Investing by myself or hiring someone?</p>
+                            </div>
+                            <div className='questions__container'>
+                                <img className='questions__image' src={retirement} alt='person working with a small model of a house' />
+                                <p className='questions__question'>Saving for retirement or education?</p>
+                            </div>
+                            <div className='questions__container'>
+                                <img className='questions__image' src={taxes} alt='person working with a small model of a house' />
+                                <p className='questions__question'>Using accounts that have tax advantages?</p>
+                            </div>
+                        </Carousel>
                     </div>
                 </div>
                 <ActionArticle
