29add479fd2853a7f46edc3737beb2405b4e5c4b
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-05 09:50:34 -0500

    fix: modal closes if click on logo or log in/out button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,21 +18,35 @@ function Header({ isAuth, userLoggedOut }) {
         <div className='container'>
             <header className='header'>
                 <div className='header__top'>
-                    <Link to='/' className='header__logo-link'>
+                    <Link
+                        to='/'
+                        className='header__logo-link'
+                        onClick={() => setShowNav(false)}
+                    >
                         <span className='header__logo'>my Kushki</span>
                     </Link>
                     <div className='header__top-right'>
                         <Link
                             to='/login'
-                            className={`header__auth ${isAuth ? 'header__auth--hide' : ''}`}>
+                            className={`header__auth ${isAuth ? 'header__auth--hide' : ''}`}
+                            onClick={() => setShowNav(false)}
+                        >
                             Log In
                         </Link>
                         <button
                             className={`header__auth ${isAuth ? '' : 'header__auth--hide'}`}
-                            onClick={logOut}>
+                            onClick={() => {
+                                logOut()
+                                setShowNav(false)
+                            }}>
                             Log Out
                         </button>
-                        <Hamburger toggled={isShowingNav} toggle={setShowNav} color='#fff' rounded label='Show navigation menu' />
+                        <Hamburger
+                            toggled={isShowingNav}
+                            toggle={setShowNav}
+                            color='#fff'
+                            rounded label='Show navigation menu'
+                        />
                     </div>
                 </div>
                 <div className='header__nav'>
