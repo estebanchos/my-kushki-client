e0a83f199aac07c6ad81e78a189351dbf0c224b7
Carlos Ocampo <charlie@carlosocampo.ca>
2022-07-05 23:51:22 -0500

    disabled chat feat for future implementation

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,80 +1,82 @@
-import './Chat.scss';
-import { useEffect, useState } from 'react';
-import ScrollToBottom from 'react-scroll-to-bottom';
-// import { apiUrl } from '../../utils/api';
+//  ### for implementing if enough extra time
 
-// '/contactus/chat' 
+// import './Chat.scss';
+// import { useEffect, useState } from 'react';
+// import ScrollToBottom from 'react-scroll-to-bottom';
+// // import { apiUrl } from '../../utils/api';
 
-function Chat({ socket, room, name }) {
-    const [currentMessage, setCurrentMessage] = useState('')
-    const [messageList, setMessageList] = useState([])
+// // '/contactus/chat' 
 
-    const sendMessage = async () => {
-        if (currentMessage !== '') {
-            const messageData = {
-                room: room,
-                author: name,
-                message: currentMessage,
-            }
+// function Chat({ socket, room, name }) {
+//     const [currentMessage, setCurrentMessage] = useState('')
+//     const [messageList, setMessageList] = useState([])
 
-           await socket.emit('send_message', messageData)
-            setMessageList((list) => [...list, messageData]);
-            setCurrentMessage('')
-        }
-    }
+//     const sendMessage = async () => {
+//         if (currentMessage !== '') {
+//             const messageData = {
+//                 room: room,
+//                 author: name,
+//                 message: currentMessage,
+//             }
 
-    useEffect(() => {
-        socket.on('receive_message', (data) => {
-            console.log(data)
-            setMessageList((list) => [...list, data]);
-        });
-    }, [socket]);
+//            await socket.emit('send_message', messageData)
+//             setMessageList((list) => [...list, messageData]);
+//             setCurrentMessage('')
+//         }
+//     }
 
-    return (
-        <div className={`chat`}>
-            <div className='chat__header'>
-                <p className='chat__header-title'>Live Chat</p>
-            </div>
-            <div className='chat__body'>
-                <ScrollToBottom className='chat__container'>
-                    {messageList.map((message, index) => {
-                        return (
-                            <div
-                                key={index}
-                                className={`chat__message ${message.author === name ? 'chat__message--me' : 'chat__message--other'}`}
-                            >
-                                <div className={`message ${message.author === name ? 'message--me' : 'message--other'}`}>
-                                    <div className={`message__content ${message.author === name ? '' : 'message__content--other'}`}>
-                                        <p className={`message__text`}>
-                                            {message.message}
-                                        </p>
-                                    </div>
-                                    <div className='message__meta'>
-                                        <p className={`message__time ${message.author === name ? '' : 'message__time--other'}`}>
-                                            {message.time}
-                                        </p>
-                                        <p className={`message__author ${message.author === name ? '' : 'message_author--other'}`}>
-                                            {message.author === name ? 'Me' : message.author}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </ScrollToBottom>
-            </div >
-            <div className='chat__footer'>
-                <input
-                    className='chat__input'
-                    value={currentMessage}
-                    placeholder='Enter message...'
-                    onChange={(e) => setCurrentMessage(e.target.value)}
-                    onKeyPress={(e) => e.key === 'Enter' ? sendMessage() : null}
-                />
-                <button onClick={sendMessage}>&#9658;</button>
-            </div>
-        </div >
-    );
-}
+//     useEffect(() => {
+//         socket.on('receive_message', (data) => {
+//             console.log(data)
+//             setMessageList((list) => [...list, data]);
+//         });
+//     }, [socket]);
 
-export default Chat;
+//     return (
+//         <div className={`chat`}>
+//             <div className='chat__header'>
+//                 <p className='chat__header-title'>Live Chat</p>
+//             </div>
+//             <div className='chat__body'>
+//                 <ScrollToBottom className='chat__container'>
+//                     {messageList.map((message, index) => {
+//                         return (
+//                             <div
+//                                 key={index}
+//                                 className={`chat__message ${message.author === name ? 'chat__message--me' : 'chat__message--other'}`}
+//                             >
+//                                 <div className={`message ${message.author === name ? 'message--me' : 'message--other'}`}>
+//                                     <div className={`message__content ${message.author === name ? '' : 'message__content--other'}`}>
+//                                         <p className={`message__text`}>
+//                                             {message.message}
+//                                         </p>
+//                                     </div>
+//                                     <div className='message__meta'>
+//                                         <p className={`message__time ${message.author === name ? '' : 'message__time--other'}`}>
+//                                             {message.time}
+//                                         </p>
+//                                         <p className={`message__author ${message.author === name ? '' : 'message_author--other'}`}>
+//                                             {message.author === name ? 'Me' : message.author}
+//                                         </p>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         )
+//                     })}
+//                 </ScrollToBottom>
+//             </div >
+//             <div className='chat__footer'>
+//                 <input
+//                     className='chat__input'
+//                     value={currentMessage}
+//                     placeholder='Enter message...'
+//                     onChange={(e) => setCurrentMessage(e.target.value)}
+//                     onKeyPress={(e) => e.key === 'Enter' ? sendMessage() : null}
+//                 />
+//                 <button onClick={sendMessage}>&#9658;</button>
+//             </div>
+//         </div >
+//     );
+// }
+
+// export default Chat;
--- src/pages/ChatPage/ChatPage.js
+++ src/pages/ChatPage/ChatPage.js
@@ -1,66 +1,68 @@
-import { useEffect, useState } from 'react';
-import Chat from '../../components/Chat/Chat';
-import Login from '../../components/LogIn/Login';
-import './ChatPage.scss';
-import io from 'socket.io-client';
-import { apiUrl } from '../../utils/api';
-import axios from 'axios';
+//  ### for implementing if enough extra time
 
-const socket = io.connect(apiUrl)
+// import { useEffect, useState } from 'react';
+// import Chat from '../../components/Chat/Chat';
+// import Login from '../../components/LogIn/Login';
+// import './ChatPage.scss';
+// import io from 'socket.io-client';
+// import { apiUrl } from '../../utils/api';
+// import axios from 'axios';
 
-function ChatPage({ isAuth, userLoggedIn }) {
-    const [showChat, setShowChat] = useState(false)
-    const [name, setName] = useState('')
-    const [room, setRoom] = useState('')
+// const socket = io.connect(apiUrl)
 
-    const joinRoom = (e) => {
-        setShowChat(true)
-    }
+// function ChatPage({ isAuth, userLoggedIn }) {
+//     const [showChat, setShowChat] = useState(false)
+//     const [name, setName] = useState('')
+//     const [room, setRoom] = useState('')
 
-    useEffect(() => {
-        let authorizedToken = sessionStorage.getItem('token')
-        if (authorizedToken) {
-            setRoom('kushki')
-            const authHeader = { Authorization: 'Bearer ' + authorizedToken }
-            axios.get(apiUrl + '/users/userInfo', { headers: authHeader })
-                .then(res => {
-                    let name = res.data
-                    setName(res.data)
-                    socket.emit('join', room)
-                })
-                .catch(err => console.error(err))
-        }
-    }, [])
+//     const joinRoom = (e) => {
+//         setShowChat(true)
+//     }
 
-    if (!isAuth) {
-        return (
-            <div className='main-unauth-dp'>
-                <div className='main-unauth-dp__container'>
-                    <h1 className='main-unauth-dp__title'>Chat with us</h1>
-                    <span className='main-unauth-dp__copy'>Please sign in to chat with us</span>
-                </div>
-                <Login userLoggedIn={userLoggedIn} link='/contactus' />
-            </div>
-        )
-    }
+//     useEffect(() => {
+//         let authorizedToken = sessionStorage.getItem('token')
+//         if (authorizedToken) {
+//             setRoom('kushki')
+//             const authHeader = { Authorization: 'Bearer ' + authorizedToken }
+//             axios.get(apiUrl + '/users/userInfo', { headers: authHeader })
+//                 .then(res => {
+//                     let name = res.data
+//                     setName(res.data)
+//                     socket.emit('join', room)
+//                 })
+//                 .catch(err => console.error(err))
+//         }
+//     }, [])
 
-    return (
-        <main className='main-chat'>
-            <h1 className='main-chat__title'>Chat with us</h1>
-            <div className={!showChat ? 'main-chat__join-container' : 'main-chat__join-container--hidden'}>
-                <button
-                    className='main-chat__join-button'
-                    onClick={joinRoom}
-                >
-                    Join now
-                </button>
-            </div>
-            <div className={!showChat ? 'main-chat__placeholder' : 'main-chat__placeholder--hidden'}></div>
-            <div className={showChat ? 'main-chat__messages-container' : 'main-chat__messages-container--hidden'}>
-                <Chat socket={socket} room={room} name={name} />
-            </div>
-        </main>
-    );
-}
+//     if (!isAuth) {
+//         return (
+//             <div className='main-unauth-dp'>
+//                 <div className='main-unauth-dp__container'>
+//                     <h1 className='main-unauth-dp__title'>Chat with us</h1>
+//                     <span className='main-unauth-dp__copy'>Please sign in to chat with us</span>
+//                 </div>
+//                 <Login userLoggedIn={userLoggedIn} link='/contactus' />
+//             </div>
+//         )
+//     }
 
-export default ChatPage;
+//     return (
+//         <main className='main-chat'>
+//             <h1 className='main-chat__title'>Chat with us</h1>
+//             <div className={!showChat ? 'main-chat__join-container' : 'main-chat__join-container--hidden'}>
+//                 <button
+//                     className='main-chat__join-button'
+//                     onClick={joinRoom}
+//                 >
+//                     Join now
+//                 </button>
+//             </div>
+//             <div className={!showChat ? 'main-chat__placeholder' : 'main-chat__placeholder--hidden'}></div>
+//             <div className={showChat ? 'main-chat__messages-container' : 'main-chat__messages-container--hidden'}>
+//                 <Chat socket={socket} room={room} name={name} />
+//             </div>
+//         </main>
+//     );
+// }
+
+// export default ChatPage;
